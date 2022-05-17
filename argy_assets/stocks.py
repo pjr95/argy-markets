0331b3476446369d39b054d314021159613fee1d
pjr95 <pablo.romero@mi.unc.edu.ar>
2022-05-16 23:53:02 -0300

    Stocks classes

--- argy_assets/stocks.py
+++ argy_assets/stocks.py
@@ -20,9 +20,31 @@ class Stock():
         self.g = g
 
 class Cedear(Stock):
-      def __init__(self, ticker,  price, foreign_ticker = '', 
+    ''' Class for modeling Cedears
+    Attributes
+    ==========
+    ticker: str
+            stock ticker
+    price: float
+           stock price
+    ratio: int
+           ratio of Cedears that form one 
+           stock in the original exchange
+    foreign_ticker: str
+                    original stock ticker
+    foreign_market: str
+                    name of the original
+                    exchange where the stock
+                    is listed
+    lot: int
+         quoting lot
+    g: float
+       dividend rate
+    '''
+    def __init__(self, ticker,  price, ratio, foreign_ticker = '', 
                    foreign_market = '', foreign_price = 0, lot = 1, g = 0):
-          super().__init__(ticker,  price, lot, g)
-          self.foreign_ticker = foreign_ticker 
-          self.foreign_market = foreign_market
-          self.foreign_price = foreign_price 
+        super().__init__(ticker,  price, lot, g)
+        self.foreign_ticker = foreign_ticker 
+        self.foreign_market = foreign_market
+        self.foreign_price = foreign_price 
+        self.ratio = ratio
